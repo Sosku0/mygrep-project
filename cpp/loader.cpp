841d760f9e05db00cfd8bb6350b3e5afd3c975df
Sosku0 <dev@sosku.fi>
2025-03-03 10:37:44 +0200

    First commit.
    Signed-off-by: Sosku0 <dev@sosku.fi>

---
+++ cpp/loader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oskar on 03/03/2025.
+//
+
+#include "loader.h"
---
+++ cpp/loader.h
@@ -0,0 +1,16 @@
+//
+// Created by oskar on 03/03/2025.
+//
+
+#ifndef LOADER_H
+#define LOADER_H
+
+
+
+class loader {
+
+};
+
+
+
+#endif //LOADER_H
